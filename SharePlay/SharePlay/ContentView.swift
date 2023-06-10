223f4d3e38b26e7c7e5c80164a5bac0be0d69820
Kiarash Asar <kia@toptal.com>
2023-06-10 16:37:40 -0700

    Basics of SharePlay

--- SharePlay/SharePlay/ContentView.swift
+++ SharePlay/SharePlay/ContentView.swift
@@ -5,11 +5,49 @@
 //  Created by Ratnesh Jain on 11/06/23.
 //
 
+import GroupActivities
 import SwiftUI
+import OSLog
+
+struct GameMessage: Codable, Identifiable {
+    let id: UUID
+    let text: String
+}
 
 struct ContentView: View {
+    let logger = Logger(subsystem: "SharePlay", category: "Logic")
+    
     var body: some View {
-        LaunchScreen()
+        VStack(spacing: 40) {
+            ShareLink(item: URL(string: "https://apple.com")!)
+            Button("Activate") {
+                activateSession()
+            }
+            Button("Join") {
+                joinSession()
+            }
+        }
+    }
+}
+
+private extension ContentView {
+    func activateSession() {
+        Task {
+            do {
+                _ = try await PlayTogether().activate()
+            } catch {
+                logger.log("Failed to activate SharePlay: \(error)")
+            }
+        }
+    }
+    func joinSession() {
+        Task {
+            for await session in PlayTogether.sessions() {
+                session.join()
+//                let messenger = GroupSessionMessenger(session: session, deliveryMode: .reliable)
+//                for await (message, context) in messenger.messages(of: GameMessage.self) { }
+            }
+        }
     }
 }
 
---
+++ SharePlay/SharePlay/GroupActivity/PlayTogether.swift
@@ -0,0 +1,18 @@
+//
+//  PlayTogether.swift
+//  SharePlay
+//
+//  Created by Kiarash Asar on 6/10/23.
+//
+
+import Foundation
+import GroupActivities
+
+struct PlayTogether: GroupActivity {
+    var metadata: GroupActivityMetadata {
+        var metadata = GroupActivityMetadata()
+        metadata.title = "Share Play"
+        metadata.type = .exploreTogether
+        return metadata
+    }
+}
