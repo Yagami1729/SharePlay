2523e278077fcc25b99ea0f8de26f298aad049a2
Ratnesh <ratneshjain1993@gmail.com>
2023-06-11 00:42:13 +0530

    Initial Commit

---
+++ SharePlay/SharePlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SharePlay
+//
+//  Created by Ratnesh Jain on 11/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SharePlay/SharePlay/SharePlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharePlayApp.swift
+//  SharePlay
+//
+//  Created by Ratnesh Jain on 11/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SharePlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
