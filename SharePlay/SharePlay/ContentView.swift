6b5a2397fffb0edc97f6ee35008e1e5a4323d99c
Kiarash Asar <kia@toptal.com>
2023-06-10 18:07:41 -0700

    SharePlay basic message delivery implemented

--- SharePlay/SharePlay/ContentView.swift
+++ SharePlay/SharePlay/ContentView.swift
@@ -12,25 +12,51 @@ import OSLog
 struct GameMessage: Codable, Identifiable {
     let id: UUID
     let text: String
+    let timestamp: Date
 }
 
 struct ContentView: View {
     let logger = Logger(subsystem: "SharePlay", category: "Logic")
+    @State private var message = ""
+    @State private var messagesHistory: [GameMessage] = []
+    @State private var model = SharePlayModel()
     
     var body: some View {
         VStack(spacing: 40) {
-            ShareLink(item: URL(string: "https://apple.com")!)
+            ShareLink(item: URL(string: "https://apple.com")!) {
+                Text("Share Link")
+            }
             Button("Activate") {
                 activateSession()
             }
             Button("Join") {
                 joinSession()
             }
+            Button("Fetch Messages") {
+                fetchMessages()
+            }
+            
+            TextField("Message", text: $message)
+                .padding()
+            Button("Send") {
+                sendMessage()
+            }
+            List {
+                ForEach(messagesHistory) { message in
+                    let timestamp = dateFormatter.string(from: message.timestamp)
+                    Text("\(timestamp): \(message.text)")
+                }
+            }
         }
     }
 }
 
 private extension ContentView {
+    var dateFormatter: DateFormatter {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "M/d/y h:mm a"
+        return dateFormatter
+    }
     func activateSession() {
         Task {
             do {
@@ -44,10 +70,37 @@ private extension ContentView {
         Task {
             for await session in PlayTogether.sessions() {
                 session.join()
-//                let messenger = GroupSessionMessenger(session: session, deliveryMode: .reliable)
-//                for await (message, context) in messenger.messages(of: GameMessage.self) { }
+                model.messenger = GroupSessionMessenger(session: session, deliveryMode: .reliable)
+                for await (message, context) in model.messenger!.messages(of: GameMessage.self) {
+                    Task { @MainActor in
+                        messagesHistory.append(message)
+                    }
+                    logger.debug("Message: \(message.text)")
+                    logger.debug("Context: \(context.source)")
+                }
             }
         }
+    }
+    func fetchMessages() {
+        Task {
+            for await (message, context) in model.messenger!.messages(of: GameMessage.self) {
+                Task { @MainActor in
+                    messagesHistory.append(message)
+                }
+                logger.debug("Message: \(message.text)")
+                logger.debug("Context: \(context.source)")
+            }
+        }
+    }
+    func sendMessage() {
+        let gameMessage = GameMessage(id: UUID(), text: message, timestamp: Date())
+        model.messenger?.send(gameMessage, completion: { error in
+            if let error = error {
+                logger.error("Failed sending message with error: \(error)")
+            } else {
+                logger.debug("Game Message sent successfully")
+            }
+        })
     }
 }
 
---
+++ SharePlay/SharePlay/SharePlayModel.swift
@@ -0,0 +1,16 @@
+//
+//  SharePlayModel.swift
+//  SharePlay
+//
+//  Created by Kiarash Asar on 6/10/23.
+//
+
+import GroupActivities
+import SwiftUI
+import Observation
+import OSLog
+
+@Observable
+class SharePlayModel {
+    var messenger: GroupSessionMessenger? = nil
+}
